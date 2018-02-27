5ea7e6d9dc1c1e688e0c78c1ebda1ab8fe423807
Enoc <etorra@gmail.com>
2018-02-27 12:48:10 -0500

    score set to 0

--- app.js
+++ app.js
@@ -15,14 +15,36 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+
+document.querySelector('.dice').style.display= 'none';
+
+document.getElementById('score-0').textContent = ' 0';
+document.getElementById('score-1').textContent = ' 0';
+document.getElementById('current-0').textContent = ' 0';
+document.getElementById('current-1').textContent = ' 0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  // 1. Random Number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  //2.Display the result
+  var diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+
+
+
+  //3.Update the round score if the rolled number was not a 1
+
+
+
+});
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
-document.querySelector('.dice').style.display='none';
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
