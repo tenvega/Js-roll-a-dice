483242f9f8624e05fd512a0c389c1136f0c873fb
Enoc <etorra@gmail.com>
2018-02-27 13:11:41 -0500

    added toggle

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 
 
-document.querySelector('.dice').style.display= 'none';
+document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textContent = ' 0';
 document.getElementById('score-1').textContent = ' 0';
 document.getElementById('current-0').textContent = ' 0';
 document.getElementById('current-1').textContent = ' 0';
 
-document.querySelector('.btn-roll').addEventListener('click', function(){
+document.querySelector('.btn-roll').addEventListener('click', function() {
   // 1. Random Number
   var dice = Math.floor(Math.random() * 6) + 1;
 
@@ -33,11 +33,27 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
   diceDOM.style.display = 'block';
   diceDOM.src = 'dice-' + dice + '.png';
 
-
-
-
   //3.Update the round score if the rolled number was not a 1
+  if (dice !== 1){
+    //Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
 
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
 });
