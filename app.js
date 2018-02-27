e1f9da634e50f7e78a8f33f89bc7800eb6479d0c
Enoc <etorra@gmail.com>
2018-02-27 15:29:29 -0500

    added init function

--- app.js
+++ app.js
@@ -10,19 +10,8 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer, dice;
+init();
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = ' 0';
-document.getElementById('score-1').textContent = ' 0';
-document.getElementById('current-0').textContent = ' 0';
-document.getElementById('current-1').textContent = ' 0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // 1. Random Number
@@ -55,7 +44,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
   //check if player won the game
-  if(scores[activePlayer] >= 20){
+  if (scores[activePlayer] >= 20) {
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
     document.querySelector('.dice').style.display = 'none';
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -86,6 +75,29 @@ function nextPlayer() {
 
   document.querySelector('.dice').style.display = 'none';
 }
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = ' 0';
+  document.getElementById('score-1').textContent = ' 0';
+  document.getElementById('current-0').textContent = ' 0';
+  document.getElementById('current-1').textContent = ' 0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.add('active');
+
+};
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
